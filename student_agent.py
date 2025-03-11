c7df4cac574d95739d3f80fc3555848da1157c2e
hsuanchi1506 <hsuanchi1506@gmail.com>
2025-03-11 15:29:47 +0800

    first update

--- student_agent.py
+++ student_agent.py
@@ -1,18 +1,49 @@
-# Remember to adjust your student ID in meta.xml
 import numpy as np
 import pickle
 import random
-import gym
+
+
+with open("q_table.pkl", "rb") as f:
+    q_table = pickle.load(f)
+
+
+passenger_on_taxi = 0
+
+def get_discrete_state(obs, passenger_on_taxi):
+    return tuple(obs) + (passenger_on_taxi,)
 
 def get_action(obs):
+    global passenger_on_taxi
+
+
+    taxi_row, taxi_col, station_0_row, station_0_col, station_1_row, station_1_col, \
+    station_2_row, station_2_col, station_3_row, station_3_col, \
+    obstacle_north, obstacle_south, obstacle_east, obstacle_west, \
+    passenger_look, destination_look = obs
+
+    stations = [(station_0_row, station_0_col), (station_1_row, station_1_col),
+                (station_2_row, station_2_col), (station_3_row, station_3_col)]
+
+    if passenger_look == 0:
+        passenger_on_taxi = 0
+
+    if passenger_look == 1 and (taxi_row, taxi_col) in stations: 
+        passenger_on_taxi = 1
+
+    state = get_discrete_state(obs, passenger_on_taxi)
+
+
+    if random.uniform(0, 1) < 0.1:
+        action = random.randint(0, 5)
+    else:
+        action = np.argmax(q_table.get(state, np.zeros(6))) 
+
+
+    if passenger_look == 0:
+        passenger_on_taxi = 0
+
+
+    if action == 4 and passenger_look == 1 and (taxi_row, taxi_col) in stations:
+        passenger_on_taxi = 1
     
-    # TODO: Train your own agent
-    # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
-    # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
-    #       Otherwise, even if your agent performs well in training, it may fail during testing.
-
-
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
-
+    return action
